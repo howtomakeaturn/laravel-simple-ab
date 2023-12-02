14f06a9e74d00cc6b3c6ac2a4a82541a6d52eb58
howtomakeaturn <howtomakeaturn@hotmail.com>
2023-12-02 09:32:23 +0800

    sticky value

--- src/Experiment.php
+++ src/Experiment.php
@@ -2,20 +2,40 @@
 
 namespace Howtomakeaturn\LaravelSimpleAB;
 
+use Cookie;
+
 class Experiment
 {
     protected $key;
 
+    protected $value;
+
     public function __construct($key)
     {
         $this->key = $key;
+
+        $values = config("laravel-simple-ab.experiments.{$this->key}");
+
+        $cookieKey = 'laravel-simple-ab-experiment-'. $this->key;
+
+        $index = Cookie::get($cookieKey);
+
+        if ($index === null) {
+            $index = rand(0, count($values) - 1);
+
+            $cookie = Cookie::forever($cookieKey, $index);
+
+            Cookie::queue($cookie);
+
+            $this->value = $values[$index];
+        } else {
+            $this->value = $values[(int) $index];
+        }
     }
 
     public function getValue()
     {
-        $values = config("laravel-simple-ab.experiments.{$this->key}");
-
-        return $values[array_rand($values)];
+        return $this->value;
     }
 
     public function increment()
