427325518e6b24dc8b4b5618a4f19f45c4826a76
howtomakeaturn <howtomakeaturn@hotmail.com>
2023-12-02 11:04:11 +0800

    track event

--- src/Experiment.php
+++ src/Experiment.php
@@ -3,6 +3,7 @@
 namespace Howtomakeaturn\LaravelSimpleAB;
 
 use Cookie;
+use File;
 
 class Experiment
 {
@@ -38,8 +39,32 @@ class Experiment
         return $this->value;
     }
 
-    public function increment()
+    public function increment($event)
     {
-        // todo
+        $path = storage_path().'/laravel-simple-ab';
+
+        File::isDirectory($path) or File::makeDirectory($path, 0777, true, true);
+
+        $filePath = $path . '/' . $this->key . '.json';
+
+        File::isFile($filePath) or File::put($filePath, '{}');
+
+        $str = File::get($filePath);
+
+        $data = json_decode($str, true);
+
+        if (array_key_exists($this->getValue(), $data)) {
+            if (array_key_exists($event, $data[$this->getValue()])) {
+                $data[$this->getValue()][$event] += 1;
+            } else {
+                $data[$this->getValue()][$event] = 1;
+            }
+        } else {
+            $data[$this->getValue()] = [
+                $event => 1,
+            ];
+        }
+
+        File::put($filePath, json_encode($data));
     }
 }
